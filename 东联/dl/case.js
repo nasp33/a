c278ca1604fc67dcd8960eca3ab3fa9ded0f8402
nasp33 <1107364603@qq.com>
2017-03-11 11:31:00 +0800

    dl

---
+++ 东联/dl/case.js
@@ -0,0 +1,53 @@
+// JavaScript Document
+$("header>div>nav>ul>li>a").hover(function(){
+		$(this).parent().stop(false,true).animate({"background-position-x":"6px",opacity:"1"},{duration:"normal", easing: "easeOutElastic"});
+	},function(){
+		$(this).parent().stop(false,true).animate({"background-position-x":"10px",opacity:"1"},{duration:"normal", easing: "easeOutElastic"});
+	});
+	<!--- Ê×Ò³ ---->
+	$('.ad_img,#banner_ctr,#client').pngFix();
+	$(window).scroll(function(){
+		$(this).scrollTop()>80?$("#navbg").stop(false,true).animate({opacity:"1"},"normal"):$("#navbg").stop(false,true).animate({opacity:"0.8"},"normal");
+	});
+	
+	//Cases Start
+	if($.support.transition){
+		$("#cases>ul>li").hover(function(){
+			$("img",this).stop(false,true).transition({
+				perspective: '300px',
+				rotateY: '180deg',
+				opacity: '0'
+			});
+			$("p",this).css({display:'block',opacity:'0',rotateY: '-180deg'}).stop(false,true).transition({
+				perspective: '300px',
+				rotateY: '0deg',
+				opacity: '1'
+			});
+		},function(){
+			$("img",this).show().stop(false,true).transition({
+				perspective: '300px',
+				rotateY: '0deg',
+				opacity: '1'
+			});
+			$("p",this).stop(false,true).transition({
+				perspective: '300px',
+				rotateY: '180deg',
+				opacity: '0'
+			});
+		});
+	}else{
+		$("#cases>ul>li").hover(function(){
+			$("img",this).stop(false,true).slideUp("fast");
+			$("p",this).stop(false,true).slideDown("fast");
+		},function(){
+			$("img",this).stop(false,true).slideDown("fast");
+			$("p",this).stop(false,true).slideUp("fast");
+		});
+	}
+	$("#cases>ul>li>img").lazyload({effect:"fadeIn",failurelimit:10});
+						$("#gotop").click(function(){$('body,html').animate({scrollTop:0},500);})
+	var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+	document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F3fe5b2b119b5fc4931e9c73e7071b0c6' type='text/javascript'%3E%3C/script%3E"));
+	var bds_config = {"bdTop":203};
+	$("#bdshell_js").attr("src","http://share.baidu.com/static/js/shell_v2.js?cdnversion=" + new Date().getHours());
+//]]>
